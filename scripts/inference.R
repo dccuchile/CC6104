2639e2598d88b92a3232ba805ad00e4e6dc028d5
Felipe <felipebravom@gmail.com>
2021-04-19 14:50:23 -0400

    inf

--- scripts/inference.R
+++ scripts/inference.R
@@ -4,24 +4,35 @@
 
 # Show that 
 
-library(gtools)
-library(tidyverse)
-sd.p=function(x){sd(x)*sqrt((length(x)-1)/length(x))}
+
+
 
 pop <-c(2,3,4,5,6)
-samp_size <- 2
-
 mean(pop)
+sd.p=function(x){sd(x)*sqrt((length(x)-1)/length(x))}
 sd.p(pop)
 
-samples<-as_tibble(permutations(length(pop), samp_size, pop, repeats.allowed=TRUE))
 
-samples <- samples %>% rowwise() %>% mutate(sample_mean=mean(c(V1,V2)))
+library(gtools)
+library(tidyverse)
+samp_size <- 2
+samples<-as_tibble(permutations(length(pop), samp_size,
+                                pop, repeats.allowed=TRUE))
+
+samples <- samples %>% rowwise() %>% 
+  mutate(sample_mean=mean(c(V1,V2)))
+samples
+
+library(ggplot2)
+ggplot(samples, aes(x=sample_mean)) + 
+  geom_histogram(bins = 9, color="black", fill="white")
+ggplot(data.frame(pop), aes(x=pop)) +
+  geom_histogram(bins = 5, color="black", fill="white")
 
 
 
 mean(samples$sample_mean)
 sd.p(samples$sample_mean)
-hist(samples$sample_mean)
+
 
 sd.p(pop)/sqrt(samp_size)
