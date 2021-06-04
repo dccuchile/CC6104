25017fe38686f63b00e0b3d06caa6678f5df5edd
Felipe <felipebravom@gmail.com>
2021-06-04 14:38:13 -0400

    update

--- scripts/regresion.R
+++ scripts/regresion.R
@@ -47,7 +47,7 @@ plot(d2$height,reg1$fitted.values)
 new.weights<-data.frame(weight=c(50,62))
 predict.lm(object=reg1,newdata=new.weights)
 # this is equivalent to:
-(reg1.coef[1]+reg1.coef[2]*new.weights)[1:2,]
+reg1.coef[1]+reg1.coef[2]*new.weights[1:2,]
 
 # Regresión Mutilple
 
