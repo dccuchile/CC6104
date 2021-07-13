24f231af962812e0bcd7ced93da34acfb2d4945a
Felipe <felipebravom@gmail.com>
2021-07-13 13:56:54 -0400

    update

--- scripts/BayesReg.R
+++ scripts/BayesReg.R
@@ -6,7 +6,8 @@ d2 <- d[ d$age >= 18 , ]
 
 b.reg1 <- quap(
   alist(
-    height ~ dnorm( b0 + b1*weight, sigma ),
+    height ~ dnorm( mu, sigma ),
+    mu <- b0 + b1*weight,
     b0 ~ dnorm( 150 , 50 ) ,
     b1 ~ dnorm( 0 , 1) ,
     sigma ~ dunif( 0 , 50 )
@@ -34,11 +35,14 @@ cov2cor( vcov( b.reg2 ) )
 # samples from the posterior
 post <- extract.samples( b.reg1, n= 1e4 )
 head(post)
+sapply(post,mean)
+
 precis(post,prob=0.95)
 
 library(MASS)
 post2 <- mvrnorm( n=1e4 , mu=coef(b.reg1 ) , Sigma=vcov(b.reg1 ) )
-head(post2)
+post2<-as.data.frame(post2)
+sapply(post2,mean)
 
 precis(as.data.frame(post2))
 
@@ -53,13 +57,8 @@ post[1:5,]
 
 round( vcov( b.reg1 ) , 3 )
 
-coef(b.reg1)
-plot( height ~ weight , data=d2 )
-  abline( a=coef(b.reg1)["b0"] , b=coef(b.reg1)["b1"] )
-
 
 plot( height ~ weight , data=d2 , col=rangi2 )
-post <- extract.samples( b.reg1 )
 b0_map <- mean(post$b0)
 b1_map <- mean(post$b1)
 curve( b0_map + b1_map*x, add=TRUE )
@@ -69,7 +68,7 @@ curve( b0_map + b1_map*x, add=TRUE )
 
 mu_at_50 <- post$b0 + post$b1 * 50
 dens( mu_at_50 , col=rangi2 , lwd=2 , xlab="mu|weight=50" )
-PI( mu_at_50 , prob=0.89 )
+HPDI( mu_at_50 , prob=0.95 )
 
 
 
@@ -83,28 +82,28 @@ weight.seq <- seq( from=25 , to=70 , by=1 )
 # a posterior distribution of mu for each weight in weight.seq
 
 
-mu <- link( b.reg1 , data=data.frame(weight=weight.seq) )
+
+
+
+# Let's implement link ourselves
+mu.link <- function(weight) post$b0 + post$b1*weight
+mu <- sapply( weight.seq , mu.link )
+dim(mu)
+head(mu)
 
 mu.mean <- apply( mu , 2 , mean )
-mu.PI <- apply( mu , 2 , PI , prob=0.89 )
-
+head(mu.mean)
+mu.HPDI <- apply( mu , 2 , HPDI , prob=0.95 )
 
 plot( height ~ weight , data=d2 , col=col.alpha(rangi2,0.5) )
 # plot the MAP line, aka the mean mu for each weight
 lines( weight.seq , mu.mean )
-# plot a shaded region for 89% PI
-shade( mu.PI , weight.seq )
+# plot a shaded region for 95% HPPDI
+shade( mu.HPDI , weight.seq )
 
-# Let's implement link ourselves
-mu.link <- function(weight) post$b0 + post$b1*weight
-mu2 <- sapply( weight.seq , mu.link )
-mu2.mean <- apply( mu2 , 2 , mean )
-mu2.PI <- apply( mu2 , 2 , PI , prob=0.95 )
-plot( height ~ weight , data=d2 , col=col.alpha(rangi2,0.5) )
-# plot the MAP line, aka the mean mu for each weight
-lines( weight.seq , mu2.mean )
-# plot a shaded region for 89% PI
-shade( mu2.PI , weight.seq )
+# The same could have beem done using the link function
+mu <- link( b.reg1 , data=data.frame(weight=weight.seq), n=1000 )
+
 
 
 # Predictor Intervals using the Posterior Predictive Distribution
@@ -112,26 +111,31 @@ shade( mu2.PI , weight.seq )
 
 #str(sim.height)
 
-weight.sim <- function(weight) 
+height.weight <- function(weight) 
   rnorm(
     n=nrow(post) ,
     mean=post$b0 + post$b1*weight ,
     sd=post$sigma )
-sim.height <- sapply( weight.seq , weight.sim)
+
+rnorm(4,mean=c(-100,100),sd=c(1,50))
+
+sim.height <- sapply( weight.seq , height.weight)
+dim(sim.height)
+
 
 #or alternatively
 #sim.height <- sim( b.reg1 , data=list(weight=weight.seq) )
 
-height.PI <- apply( sim.height , 2 , PI , prob=0.89 )
+height.HPDI <- apply( sim.height , 2 , HPDI , prob=0.95 )
 
 # plot raw data
 plot( height ~ weight , d2 , col=col.alpha(rangi2,0.5) )
 # draw MAP line
 lines( weight.seq , mu.mean )
 # draw HPDI region for line
-shade( mu.PI , weight.seq )
-# draw PI region for simulated heights
-shade( height.PI , weight.seq )
+shade( mu.HPDI , weight.seq )
+# draw HPDI region for simulated heights
+shade( height.HPDI , weight.seq )
 
 
 
