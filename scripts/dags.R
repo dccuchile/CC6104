118cc79b076c591e54c24a1295639a43787bc97b
Felipe <felipebravom@gmail.com>
2021-09-02 17:14:33 -0400

    update

--- scripts/dags.R
+++ scripts/dags.R
@@ -8,7 +8,20 @@ DMA_dag2 <- dagitty('dag{ D <- A -> M }')
 plot(DMA_dag2)
 impliedConditionalIndependencies( DMA_dag2 )
 
+over <- dagitty("dag{ o -> h; s -> h; s ->c }")
+coordinates(over) <- list( x=c(o=0,h=1,s=2,c=3) , y=c(o=0,h=1,s=0,c=1) )
+plot(over)
+parents( over, "h" )
+paths( over, "o", "c" )$path
+paths( over, "o", "c" , directed = T)$path
 
+
+dconnected( "dag{x->m->y}", "x", "y", c() ) # TRUE
+dconnected( "dag{x->m->y}", "x", "y", c("m") ) # FALSE
+dseparated( "dag{x->m->y}", "x", "y", c() ) # FALSE 
+dseparated( "dag{x->m->y}", "x", "y", c("m") ) # TRUE
+
+impliedConditionalIndependencies(over)
 
 fo <- dagitty("dag{ fo -> lo; fo -> do; bp -> do;do->hb }")
 coordinates(fo) <- list( x=c(lo=0,fo=1,do=2,hb=2,bp=3) , y=c(fo=0,bp=0,lo=1,do=1,hb=2) )
