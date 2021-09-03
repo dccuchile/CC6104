d1c681a1b5e44d56caba362d78fd00e807c08143
Felipe <felipebravom@gmail.com>
2021-09-03 16:59:03 -0400

    update

--- scripts/dags.R
+++ scripts/dags.R
@@ -1,25 +1,26 @@
 library(dagitty)
-dag5.1 <- dagitty( "dag{ A -> D; A -> M; M -> D }" )
-coordinates(dag5.1) <- list( x=c(A=0,D=1,M=2) , y=c(A=0,D=1,M=0) )
-plot( dag5.1 )
-
-
-DMA_dag2 <- dagitty('dag{ D <- A -> M }')
-plot(DMA_dag2)
-impliedConditionalIndependencies( DMA_dag2 )
 
 over <- dagitty("dag{ o -> h; s -> h; s ->c }")
 coordinates(over) <- list( x=c(o=0,h=1,s=2,c=3) , y=c(o=0,h=1,s=0,c=1) )
 plot(over)
+
+library(ggdag)
+tidy_dagitty(over)
+ggdag(over, layout="circle")
+
+
+
+children(over,"s")
 parents( over, "h" )
 paths( over, "o", "c" )$path
-paths( over, "o", "c" , directed = T)$path
 
+paths( over, "s", "c",directed = T )$path
 
-dconnected( "dag{x->m->y}", "x", "y", c() ) # TRUE
-dconnected( "dag{x->m->y}", "x", "y", c("m") ) # FALSE
-dseparated( "dag{x->m->y}", "x", "y", c() ) # FALSE 
-dseparated( "dag{x->m->y}", "x", "y", c("m") ) # TRUE
+impliedConditionalIndependencies(over)
+dconnected(over,"o","s",c())
+dconnected(over,"o","s",c("h"))
+dseparated(over,"o","s",c("h"))
+
 
 impliedConditionalIndependencies(over)
 
@@ -29,7 +30,5 @@ coordinates(fo) <- list( x=c(lo=0,fo=1,do=2,hb=2,bp=3) , y=c(fo=0,bp=0,lo=1,do=1
     plot(fo)
 impliedConditionalIndependencies(fo)
 
-library(ggdag)
-tidy_dagitty(fo)
-ggdag(fo, layout = "circle")
+
 
