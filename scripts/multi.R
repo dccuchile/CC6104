c0ca0006a1f13ff71badaf2bc9edd32ee58b9169
Felipe <felipebravom@gmail.com>
2021-09-13 12:06:07 -0300

    update

---
+++ scripts/multi.R
@@ -0,0 +1,47 @@
+
+### GLMS
+library(rethinking)
+data(chimpanzees)
+d <- chimpanzees
+
+d$treatment <- 1 + d$prosoc_left + 2*d$condition
+
+m11.1 <- quap(
+  alist(
+    pulled_left ~ dbinom( 1 , p ) ,
+    logit(p) <- a ,
+    a ~ dnorm( 0 , 10 )
+  ) , data=d )
+
+m11.2 <- quap(
+  alist(
+    pulled_left ~ dbinom( 1 , p ) ,
+    logit(p) <- a + b[treatment] ,
+    a ~ dnorm( 0 , 1.5 ),
+    b[treatment] ~ dnorm( 0 , 10 )
+  ) , data=d )
+set.seed(1999)
+
+
+dat_list <- list(
+  pulled_left = d$pulled_left,
+  actor = d$actor,
+  treatment = as.integer(d$treatment) )
+
+
+m11.4 <- ulam(
+  alist(
+    pulled_left ~ dbinom( 1 , p ) ,
+    logit(p) <- a[actor] + b[treatment] ,
+    a[actor] ~ dnorm( 0 , 1.5 ),
+    b[treatment] ~ dnorm( 0 , 0.5 )
+  ) , data=dat_list , chains=4 , log_lik=TRUE )
+precis( m11.4 , depth=2 )
+
+
+#### Multi-level
+
+data(reedfrogs)
+d <- reedfrogs
+str(d)
+
