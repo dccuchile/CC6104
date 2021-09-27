fa6dab934496e4d1dd111d9ebe26325364bd53cb
Felipe <felipebravom@gmail.com>
2021-09-27 17:24:21 -0300

    update

--- scripts/eval.R
+++ scripts/eval.R
@@ -9,8 +9,23 @@ d <- data.frame( species=sppnames , brain=brainvolcc,
                  mass=masskg )
 
 
-m6.1 <- lm( brain ~ mass , data=d )
-1 - var(resid(m6.1))/var(d$brain)
+reg.ev.1 <- lm( brain ~ mass , data=d )
+reg.ev.2 <- lm( brain ~ mass + I(mass^2)
+                , data=d )
+reg.ev.3 <- lm( brain ~ mass + I(mass^2)
+                + I(mass^3),data=d )
+reg.ev.4 <- lm( brain ~ mass + I(mass^2)
+                + I(mass^3) + I(mass^4),data=d )
+reg.ev.5 <- lm( brain ~ mass + I(mass^2)
+                + I(mass^3) + I(mass^4)
+                + I(mass^5),data=d )
+reg.ev.6 <- lm( brain ~ mass + I(mass^2)
+                + I(mass^3) + I(mass^4)+ 
+                  I(mass^5)+ I(mass^6),data=d )
 
-m6.2 <- lm( brain ~ mass + I(mass^2) , data=d )
-m6.3 <- lm( brain ~ mass + I(mass^2)+ I(mass^3) + I(mass^4),data=d )
+summary(reg.ev.1)$r.squared
+summary(reg.ev.2)$r.squared
+summary(reg.ev.3)$r.squared
+summary(reg.ev.4)$r.squared
+summary(reg.ev.5)$r.squared
+summary(reg.ev.6)$r.squared
