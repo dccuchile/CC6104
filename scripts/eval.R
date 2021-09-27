1ea3447c7fc8da9bd33f767365babba37938a33c
Felipe <felipebravom@gmail.com>
2021-09-27 16:38:04 -0300

    update

--- scripts/eval.R
+++ scripts/eval.R
@@ -1,8 +1,12 @@
-sppnames <- c( "afarensis","africanus","habilis","boisei",
-               "rudolfensis","ergaster","sapiens")
-brainvolcc <- c( 438 , 452 , 612, 521, 752, 871, 1350 )
-masskg <- c( 37.0 , 35.5 , 34.5 , 41.5 , 55.5 , 61.0 , 53.5 )
-d <- data.frame( species=sppnames , brain=brainvolcc , mass=masskg )
+sppnames <- c( "afarensis","africanus","habilis",
+               "boisei", "rudolfensis","ergaster",
+               "sapiens")
+brainvolcc <- c( 438 , 452 , 612, 521, 752, 871, 
+                 1350 )
+masskg <- c( 37.0 , 35.5 , 34.5 , 41.5 , 55.5 , 
+             61.0 , 53.5 )
+d <- data.frame( species=sppnames , brain=brainvolcc,
+                 mass=masskg )
 
 
 m6.1 <- lm( brain ~ mass , data=d )
