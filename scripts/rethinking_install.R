42702be389cab4ddfe5d53cfce1613e4f366f37c
felipebravom <felipebravom@gmail.com>
2022-08-22 10:41:18 -0400

    update

---
+++ scripts/rethinking_install.R
@@ -0,0 +1,12 @@
+
+
+
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+# we recommend running this is a fresh R session or restarting your current session
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+cmdstanr::install_cmdstan()
+
+install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
+devtools::install_github("rmcelreath/rethinking")
