a4773f37208df38bc03de87e75181877d4f9f1b5
felipebravom <felipebravom@gmail.com>
2023-11-13 14:14:41 -0300

    update

--- scripts/rethinking_install.R
+++ scripts/rethinking_install.R
@@ -8,5 +8,10 @@ install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOpt
 
 cmdstanr::install_cmdstan()
 
-install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"), dependencies = TRUE)
-devtools::install_github("rmcelreath/rethinking")
+#install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"), dependencies = TRUE)
+#devtools::install_github("rmcelreath/rethinking")
+
+# alternatively
+install.packages(c("mvtnorm","loo","coda"), repos="https://cloud.r-project.org/",dependencies=TRUE)
+options(repos=c(getOption('repos'), rethinking='http://xcelab.net/R'))
+install.packages('rethinking',type='source')
